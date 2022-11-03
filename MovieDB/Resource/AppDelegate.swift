934e916c22c552362d97fac8352ec73be49fbe91
Le Nguyen Thanh Trung <le.nguyen.thanh.trung@sun-asterisk>
2022-11-03 15:20:32 +0700

    [Feature] [55628] Init Project + [55592] Apply MVC Architecture

---
+++ MovieDB/Resource/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MovieDB
+//
+//  Created by le.n.t.trung on 03/11/2022.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ MovieDB/ViewController/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by le.n.t.trung on 03/11/2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
