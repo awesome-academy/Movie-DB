57e4fef442b5756d978e7d0feb38e7ea6b449e2f
Le Nguyen Thanh Trung <le.nguyen.thanh.trung@sun-asterisk>
2022-11-03 15:20:32 +0700

    [Feature] [55628] Init Project + [55592] Apply MVC Architecture

---
+++ MovieDB/Manager/CoreDataManager.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataManager.swift
+//  MovieDB
+//
+//  Created by le.n.t.trung on 03/11/2022.
+//
+
+import Foundation
+import CoreData
+
+final class PersistentContainer {
+    
+    static let shared =  PersistentContainer()
+    private var persistentContainer: NSPersistentContainer?
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "Model")
+        persistentContainer?.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+    
+    func saveContext () {
+        let context = persistentContainer?.viewContext
+        if ((context?.hasChanges) != nil) {
+            do {
+                try context?.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
