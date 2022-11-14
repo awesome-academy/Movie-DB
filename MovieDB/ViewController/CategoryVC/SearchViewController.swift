ccbab5aa66b96ed1946fb36ff27502a4591c9ed7
Le Nguyen Thanh Trung <le.nguyen.thanh.trung@sun-asterisk>
2022-11-14 16:52:21 +0700

    [Feature] [55609] Handle Logic + [55610] Fetch data Search Screen

--- MovieDB/ViewController/CategoryVC/SearchViewController.swift
+++ MovieDB/ViewController/CategoryVC/SearchViewController.swift
@@ -20,6 +20,8 @@ final class SearchViewController: UIViewController {
     private let filmRepository = FilmRepository()
     private var network = Network.shared
     private var selectedGenre: Genre?
+    private var oldIndexPathItemSelected: IndexPath?
+    private let delayRunner = DelayedRunner.initWithDuration(seconds: 0.5)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,13 +34,15 @@ final class SearchViewController: UIViewController {
     private func configView() {
         searchView.clipsToBounds = true
         searchView.layer.cornerRadius = 10
+        configActivityIndicator()
+        oldIndexPathItemSelected = IndexPath(row: 0, section: 0)
     }
     
     private func registerInit() {
+        searchTextField.delegate = self
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.backgroundColor = .black
-        
         collectionView.register(UINib(nibName: HorizontalCustomCollectionViewCell.identifier,
                                       bundle: nil),
                                 forCellWithReuseIdentifier: HorizontalCustomCollectionViewCell.identifier)
@@ -96,8 +100,13 @@ final class SearchViewController: UIViewController {
     }
     
     private func initListFilm() {
-        let url = network.getCategoryListURL(path: CategoryPath.topRated)
-        filmRepository.getAllFilm(urlString: url) { [weak self] result in
+        loadFilmsByQuery(url: network.getFilmsByGenreURL(), queryKey: "with_genres", queryValue: "")
+    }
+    
+    private func loadFilmsByQuery(url: String, queryKey: String, queryValue: String) {
+        filmRepository.getFilmsByQuery(urlString: url,
+                                       queryKey: queryKey,
+                                       queryValue: queryValue) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let data):
@@ -105,12 +114,15 @@ final class SearchViewController: UIViewController {
                 DispatchQueue.main.async {
                     self.films = data
                     self.films.convertGenresToString(genres: self.genres)
-                    self.collectionView.reloadData()
+                    self.collectionView.reloadSections(IndexSet(integer: 1))
                 }
             case .failure(let error):
                 DispatchQueue.main.async {
                     self.showAlert(title: "ERROR", messageError: error.localizedDescription)
                 }
+            }
+            DispatchQueue.main.async {
+                self.handleIndicator(type: IndicatorType.hide)
             }
         }
     }
@@ -150,6 +162,70 @@ extension SearchViewController: UICollectionViewDataSource {
 
 extension SearchViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            reloadGenresCollectionView(indexPath: indexPath)
+            searchTextField.text?.removeAll()
+            searchTextField.resignFirstResponder()
+            delayRunner.run {
+                DispatchQueue.main.async {
+                    self.handleIndicator(type: IndicatorType.show)
+                    self.loadFilmsByQuery(url: self.network.getFilmsByGenreURL(),
+                                          queryKey: "with_genres",
+                                          queryValue: indexPath.row == 0
+                                          ? ""
+                                          : String(self.genres[indexPath.row].id))
+                }
+            }
+        }
     }
 }
 
+extension SearchViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    public func textField(_ textField: UITextField,
+                          shouldChangeCharactersIn range: NSRange,
+                          replacementString string: String) -> Bool {
+        guard let oldText = textField.text,
+              let range = Range(range, in: oldText) else {
+            return true
+        }
+        let newText = oldText.replacingCharacters(in: range, with: string)
+        delayRunner.run {
+            DispatchQueue.main.async {
+                self.reloadFirstItem()
+                self.handleIndicator(type: IndicatorType.show)
+                if newText.isEmpty {
+                    self.initListFilm()
+                } else {
+                    self.loadFilmsByQuery(url: self.network.getSearchURL(), queryKey: "query", queryValue: newText)
+                }
+            }
+        }
+        return true
+    }
+}
+
+extension SearchViewController {
+    private func reloadFirstItem() {
+        reloadGenresCollectionView(indexPath: IndexPath(row: 0, section: 0))
+    }
+    
+    private func reloadGenresCollectionView(indexPath: IndexPath) {
+        selectedGenre = genres[indexPath.row]
+        collectionView.reloadItems(at: [indexPath])
+        if let oldIndexPath = oldIndexPathItemSelected {
+            collectionView.reloadItems(at: [oldIndexPath])
+        }
+        oldIndexPathItemSelected = indexPath
+        collectionView.scrollToItem(
+            at: IndexPath(item: indexPath.row, section: 0),
+            at: .centeredHorizontally,
+            animated: true
+        )
+    }
+
+}
